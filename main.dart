568159cf4fab3bf2708e5f457bbc4b7edfe451a7
Myoungmin <miung222@gmail.com>
2023-01-17 21:17:52 +0900

    int와 double의 부모 클래스인 num

--- main.dart
+++ main.dart
@@ -1,9 +1,4 @@
 void main() {
-  late final String name;
-  // API로 받아야할 필요가 있는 경우 처럼 필요한 데이터가 아직 없을 수 있을 때,
-  // 데이터 없이 변수를 만들어주고,
-  //나중에 데이터를 얻었을 때 변수에 넣어준다.
-  name = 'wakgood';
-  // final 변수이므로 아래와같이 대입하지 못한다.
-  //name = 'WooWakGood';
+  num x = 3;
+  x = 1.23;
 }
