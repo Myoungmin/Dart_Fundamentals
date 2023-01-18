1ea037ec0a519e3212a783b80e14cad5227ba47c
Myoungmin <miung222@gmail.com>
2023-01-18 20:56:55 +0900

    Collection if 실습

--- main.dart
+++ main.dart
@@ -1,4 +1,12 @@
 void main() {
-  num x = 3;
-  x = 1.23;
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    // Collection if
+    if (giveMeFive) 5,
+  ];
+  print(numbers.last); // 5 출력
 }
