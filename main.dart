c5553c4000e0bd2ad165b89d4959688e8cc1d19c
Myoungmin <miung222@gmail.com>
2023-01-21 10:33:29 +0900

    Map 실습

--- main.dart
+++ main.dart
@@ -1,11 +1,15 @@
 void main() {
-  var oldFriends = ['nico', 'lynn'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    // Collection For
-    for (var friend in oldFriends) "old $friend",
-  ];
-  print(newFriends);
+  // Map<String, Object> 자료형 유추
+  var Player = {
+    'name': 'wakgood',
+    'xp': 19.99,
+    'superpower': false,
+  };
+
+  // Map 명시적으로 선언
+  Map<List<int>, bool> AreTrue = {
+    [1, 2, 3, 5]: true,
+    [1, 2]: false,
+    [1, 3, 5]: true,
+  };
 }
