775166f1ab6f4899fdb2b3677c458e4cae49c192
Myoungmin <miung222@gmail.com>
2023-01-21 12:17:27 +0900

    Named Parameters 실습
    1. default parameter
    2. required

--- main.dart
+++ main.dart
@@ -1,5 +1,20 @@
-String sayHello(String name) => "Hello $name nice to meet you!";
+// String sayHello({String? name, int? age, String? country}) =>
+//     "Hello $name, you are $age, and you come from $country";
+
+// String sayHello(
+//         {String name = 'NoName', int age = 0, String country = 'NoCountry'}) =>
+//     "Hello $name, you are $age, and you come from $country";
+
+// required를 명시하면 Dart는 항상 sayHello함수를 호출할 때 반드시 파라미터를 설정해야하는 것을 알게되어, null safety 처리가 가능하다.
+String sayHello(
+        {required String name, required int age, required String country}) =>
+    "Hello $name, you are $age, and you come from $country";
 
 void main() {
-  print(sayHello('wakgood'));
+  print(sayHello(
+    age: 37,
+    country: 'Korea',
+    name: 'wakgood',
+  ));
+  //print(sayHello());
 }
