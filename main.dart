dda97660d0f9865322e43dec1f015d904d2c835c
Myoungmin <miung222@gmail.com>
2023-01-21 10:06:42 +0900

    Collection For 실습

--- main.dart
+++ main.dart
@@ -1,13 +1,11 @@
 void main() {
-  // String Interpolation 실습
-  // 1. 변수가 이미 존재할 때 사용하는 방식
-  var name = 'wakgood';
-  var greeting = 'Hello everyone, my name is $name, nice to meet you!';
-  print(greeting);
-
-  // 2. 계산을 실행할 때 문법
-  var age = 34;
-  var greeting2 =
-      "Hello everyone, my name is $name, nice to meet you! I'm ${age + 3}";
-  print(greeting2);
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    // Collection For
+    for (var friend in oldFriends) "old $friend",
+  ];
+  print(newFriends);
 }
