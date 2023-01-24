bd6425962568215fdda9ff208e2805a6f47789d8
Myoungmin <miung222@gmail.com>
2023-01-24 11:07:20 +0900

    typedef 실습

--- main.dart
+++ main.dart
@@ -1,13 +1,16 @@
-// QQ Operator의 활용 : 사용자가 함수에 null을 대입할 수 있도록 한다.
-String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
+typedef ListOfInts = List<int>;
+typedef UserInfo = Map<String, String>;
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
 
 void main() {
-  capitalizeName('wakgood');
-  capitalizeName(null);
-
-  // QQ assignment operator : name 이 null이라면 값을 할당하도록 한다.
-  String? name;
-  name ??= 'wakgood';
-  name ??= 'another';
-  print(name);
+  print(reverseListOfNumbers([1, 2, 3]));
+  print(sayHi({"name": "wakgood"}));
 }
