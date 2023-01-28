85b444a773bcf1f237ccdeee74aba4ad46723076
Myoungmin <miung222@gmail.com>
2023-01-28 10:25:37 +0900

    Named Constructors  실습
    콜론 뒤에  property 초기화 설정을 할 수 있다.

--- main.dart
+++ main.dart
@@ -1,21 +1,39 @@
 class Player {
   final String name;
   int xp;
+  String team;
 
   Player({
     required this.name,
     required this.xp,
+    required this.team,
   });
 
+  Player.createBluePlayer({required String name, required int xp})
+      : this.name = name,
+        this.xp = xp,
+        this.team = 'blue';
+
+  Player.createRedPlayer({required String name, required int xp})
+      : this.name = name,
+        this.xp = xp,
+        this.team = 'red';
+
   void sayHello() {
-    print("Hi my name is $name");
+    print("Hi my name is $name. My team is $team.");
   }
 }
 
 void main() {
-  var player = Player(
+  var bluePlayer = Player.createBluePlayer(
     name: "wakgood",
+    xp: 177,
+  );
+
+  var redPlayer = Player.createRedPlayer(
+    name: "Ine",
     xp: 158,
   );
-  player.sayHello();
+
+  bluePlayer.sayHello();
 }
