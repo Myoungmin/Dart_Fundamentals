876429fd13c832ede7e5fefff23513f8ebdbc343
Myoungmin <miung222@gmail.com>
2023-01-29 11:08:53 +0900

    Cascade Notation 실습

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 class Player {
-  final String name;
+  String name;
   int xp;
   String team;
 
@@ -9,31 +9,19 @@ class Player {
     required this.team,
   });
 
-  Player.createBluePlayer({required String name, required int xp})
-      : this.name = name,
-        this.xp = xp,
-        this.team = 'blue';
-
-  Player.createRedPlayer({required String name, required int xp})
-      : this.name = name,
-        this.xp = xp,
-        this.team = 'red';
-
   void sayHello() {
     print("Hi my name is $name. My team is $team.");
   }
 }
 
 void main() {
-  var bluePlayer = Player.createBluePlayer(
-    name: "wakgood",
-    xp: 177,
-  );
+  var wakgood = Player(name: 'wakgood', xp: 177, team: 'red');
 
-  var redPlayer = Player.createRedPlayer(
-    name: "Ine",
-    xp: 158,
-  );
-
-  bluePlayer.sayHello();
+  // Cascade Operator
+  // 작성한 코드와 똑같은 일을 하라고 명시
+  var wakgood2 = wakgood
+    ..name = 'wakgood2'
+    ..xp = 178
+    ..team = 'blue'
+    ..sayHello();
 }
