b86bfccf411101b856e624ecc50745814ccff3a7
Myoungmin <miung222@gmail.com>
2023-02-02 21:45:51 +0900

    Mixin 실습

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
+class Strong {
+  final double strengthLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("ruuuun!");
+  }
+}
+
 class Human {
   final String name;
   Human(this.name);
@@ -8,12 +18,20 @@ class Human {
 
 enum Team { red, blue }
 
-class Player extends Human {
+// with로 Mixin 적용
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+class Player extends Human with Strong, QuickRunner {
   final Team team;
 
   // 상속을 받은 클래스 생성자 형태
   // 부모의 생성자로 name을 넘겨준다.
-  Player({required this.team, required String name}) : super(name);
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name);
 
   // 메서드 오버라이드
   // super로 부모 메서드 호출
@@ -27,4 +45,5 @@ class Player extends Human {
 void main() {
   var player = Player(team: Team.red, name: 'wakgood');
   player.sayHello();
+  player.runQuick();
 }
