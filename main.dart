9e556403dd782c8fa3e17f9eeddedb4c4c65284e
AzureAD\고명민 <miung222@gmail.com>
2023-12-01 21:36:16 +0900

    then(): 비동기 실행이 성공했을 시와 결과값을 받을 수 있는 메서드

--- main.dart
+++ main.dart
@@ -1,49 +1,14 @@
-class Strong {
-  final double strengthLevel = 1500.99;
-}
+void main(){
+  print(100);
 
-class QuickRunner {
-  void runQuick() {
-    print("ruuuun!");
-  }
-}
 
-class Human {
-  final String name;
-  Human(this.name);
-  void sayHello() {
-    print("Hi my name is $name");
-  }
-}
+  // 비동기 실행이므로, 300이 먼저 출력된 후, 200이 3초 후 출력된다.
+  // 비동기 실행이 완료된 후, WhenComplete가 출력된다.
+  var future = Future<int>.delayed(Duration(seconds: 3), () => 200);
+  future.then((value) => print(value)).whenComplete((){
+    print("whenComplete");
+  });
 
-enum Team { red, blue }
 
-// with로 Mixin 적용
-class Horse with Strong, QuickRunner {}
-
-class Kid with QuickRunner {}
-
-class Player extends Human with Strong, QuickRunner {
-  final Team team;
-
-  // 상속을 받은 클래스 생성자 형태
-  // 부모의 생성자로 name을 넘겨준다.
-  Player({
-    required this.team,
-    required String name,
-  }) : super(name);
-
-  // 메서드 오버라이드
-  // super로 부모 메서드 호출
-  @override
-  void sayHello() {
-    super.sayHello();
-    print('and I play for ${team}');
-  }
-}
-
-void main() {
-  var player = Player(team: Team.red, name: 'wakgood');
-  player.sayHello();
-  player.runQuick();
+  print(300);
 }
