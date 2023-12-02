6fac2d5d37dace6c301cde5c8a708824fa9aea88
AzureAD\고명민 <miung222@gmail.com>
2023-12-03 08:51:29 +0900

    async로 비동기로 함수를 선언하고, async로 선언된 비동기 메서드 안에서, 동기적 실행을 위해 await 키워드를 사용해 명시적으로 기다릴 수 있음
    async는 비동기 메서드 {} 괄호 전에 선언하며, await는 기다려야 하는 메서드 바로 전에 선언해야 한다.

--- main.dart
+++ main.dart
@@ -1,15 +1,11 @@
 void main() {
-  print(100);
+  var future = Future<int>.delayed(Duration(seconds: 3), () => 1);
 
-  // 비동기 실행이므로, 300이 먼저 출력된 후, exception이 나게되면, Error가 출력되고,
-  // 비동기 실행이 완료된 후, WhenComplete가 출력됨
-  var future =
-      Future<int>.delayed(Duration(seconds: 3), () => throw Exception());
-  future.then((value) => print(value)).catchError((onError) {
-    print(onError);
-  }).whenComplete(() {
-    print("WhenComplete");
+  future.then((value) async {
+    print(value);
+    await Future.delayed(Duration(seconds: 1), () => print('inside await'));
+    print('after await');
   });
 
-  print(300);
+  print('last line');
 }
