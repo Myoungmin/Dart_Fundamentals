77094ef43eac3f1cd52dc9303cc430baffaa94ab
AzureAD\고명민 <miung222@gmail.com>
2023-12-02 09:36:04 +0900

    catchError(): 비동기 실행이 실패했을 시 에러 값을 받을 수 있는 메서드

--- main.dart
+++ main.dart
@@ -1,14 +1,15 @@
-void main(){
+void main() {
   print(100);
 
-
-  // 비동기 실행이므로, 300이 먼저 출력된 후, 200이 3초 후 출력된다.
-  // 비동기 실행이 완료된 후, WhenComplete가 출력된다.
-  var future = Future<int>.delayed(Duration(seconds: 3), () => 200);
-  future.then((value) => print(value)).whenComplete((){
-    print("whenComplete");
+  // 비동기 실행이므로, 300이 먼저 출력된 후, exception이 나게되면, Error가 출력되고,
+  // 비동기 실행이 완료된 후, WhenComplete가 출력됨
+  var future =
+      Future<int>.delayed(Duration(seconds: 3), () => throw Exception());
+  future.then((value) => print(value)).catchError((onError) {
+    print(onError);
+  }).whenComplete(() {
+    print("WhenComplete");
   });
-
 
   print(300);
 }
